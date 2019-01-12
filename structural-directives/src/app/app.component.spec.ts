5ccba8907727477f5b86677c82a9d369eb499ec8
Renniej <tai.juan2@gmail.com>
2019-01-12 18:39:55 -0500

    ngIf directives

--- structural-directives/src/app/app.component.spec.ts
+++ structural-directives/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'structural'`, () => {
+  it(`should have as title 'structural-directives'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('structural');
+    expect(app.title).toEqual('structural-directives');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to structural!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to structural-directives!');
   });
 });
--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -2,11 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+    <div *ngIf="displayName; then thenBlock else elseBlock"></div>
+     
+
+
+    <ng-template #thenBlock>
+    <h2>
+        {{name}}
+    </h2>
+
+  </ng-template>
+
+    <ng-template #elseBlock>
+    
+      <h2>
+          Name is hidden
+      </h2>
+
+    </ng-template>
+
+
+  `,
+  styles: [``]
 })
 export class TestComponent implements OnInit {
 
+
+  public name = "TAI R3NNie"
+  public displayName = false;
   constructor() { }
 
   ngOnInit() {
