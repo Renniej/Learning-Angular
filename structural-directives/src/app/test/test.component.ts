5bc4d3e3423ae8bd860955c5181d1513c51c388d
Renniej <tai.juan2@gmail.com>
2019-01-13 11:35:33 -0500

    Learned NgFor, ngSwitch, ngIf then else

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -3,24 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
+
     <div *ngIf="displayName; then thenBlock else elseBlock"></div>
-     
+
+      <ng-template #thenBlock>
+        <h2>
+            {{name}}
+        </h2>
+      </ng-template>
+
+      <ng-template #elseBlock>
+        <h2>
+            Name is hidden
+        </h2>
+      </ng-template>
 
 
-    <ng-template #thenBlock>
-    <h2>
-        {{name}}
-    </h2>
 
-  </ng-template>
+      <div *ngFor="let title of names; index as i; first as isFirst"> <!--intiate variable and let it represent one in element in the names array-->
+        
+        <h2>{{i}} {{isFirst}} {{title}}</h2>
+      
+      </div>
 
-    <ng-template #elseBlock>
-    
-      <h2>
-          Name is hidden
-      </h2>
+    <div [ngSwitch]="color">
 
-    </ng-template>
+      <div *ngSwitchCase="'red'"> You picked red</div>
+      <div *ngSwitchCase="'green'"> You picked green</div>
+      <div *ngSwitchCase="'blue'"> You picked blue</div>
+      <div *ngSwitchDefault>Pick again</div>
+
+    </div>
+
+
 
 
   `,
@@ -28,6 +43,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public color = "green";
+  public names = ['dee', 'trist', 'ann', 'moris', 'king kong'];
 
   public name = "TAI R3NNie"
   public displayName = false;
